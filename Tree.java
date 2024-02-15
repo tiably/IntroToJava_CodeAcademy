3c565a46cad8efb77655e3f7d4c3138483db5ba9
Tia Bly <102763623+tiably@users.noreply.github.com>
2024-02-15 11:14:34 -0500

    Add files via upload

---
+++ Tree.java
@@ -0,0 +1,18 @@
+public class Tree {
+
+	public static void main(String[] args) {
+	    //Introduce myself and print tree to screen
+	    System.out.println("Hey there, I'm Tia, and I'm learning how to code in Java!");
+	    System.out.println("I'm going to plan a tree today!");
+	    System.out.println("    #   ");
+	    System.out.println("   ###   ");
+	    System.out.println("  #####   ");
+	    System.out.println("  #####   ");
+	    System.out.println(" ####### ");
+	    System.out.println(" ####### ");
+	    System.out.println("    #  ");
+	    System.out.println("    #  ");   
+	    System.out.println("~~~~~~~~~");   
+	  }
+	
+}
